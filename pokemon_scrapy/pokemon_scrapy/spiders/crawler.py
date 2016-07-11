a61441206691c587e5a2c2c224ec7742cdceb742
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-07-10 17:14:02 -0700

    Update BeautifulSoup selector

--- pokemon_scrapy/pokemon_scrapy/spiders/crawler.py
+++ pokemon_scrapy/pokemon_scrapy/spiders/crawler.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 
 
 class PokemonCrawler(scrapy.Spider):
-	name = 'pokemon_crawler'
+	name       = 'pokemon_crawler'
 	start_urls = ['http://mpokemon.com/en/oras/pokemon.php?no=1']
 
 	def parse(self, response):
 		res = BeautifulSoup(response.body, "lxml")
-		for pokemon in res.select('#area'):
-			print pokemon
+		print res.select('#maincontent')[0].prettify() # pokemon details
+		print res.select('#maincontent2')[0].prettify() # available skills
