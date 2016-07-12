9a81057468bce6f6594187538940742c3b75b3b2
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-07-12 13:04:25 -0700

    Fix evolution chain

--- pokemon_scrapy/pokemon_scrapy/spiders/crawler.py
+++ pokemon_scrapy/pokemon_scrapy/spiders/crawler.py
@@ -12,32 +12,38 @@ class PokemonCrawler(scrapy.Spider):
 
 		for pokemon in soup.select('.infocard-tall'):
 			pokemonURL = domain + pokemon.a.attrs['href']
-			yield scrapy.Request(pokemonURL, self.parse_detail)
-		# yield scrapy.Request("http://pokemondb.net/pokedex/Ivysaur", self.parse_detail)
+			# yield scrapy.Request(pokemonURL, self.parse_detail)
+		yield scrapy.Request("http://pokemondb.net/pokedex/diancie", self.parse_detail)
 
 	def parse_detail(self, response):
-		soup           = BeautifulSoup(response.body, "lxml")
-		pokemonName    = soup.select('.svtabs-tab')[0].text
-		nationalNumber = soup.select('.vitals-table')[0].tbody.findAll('tr')[0].td.text
+		soup             = BeautifulSoup(response.body, "lxml")
+		pokemonName      = soup.select('.svtabs-tab')[0].text
+		nationalNumber   = soup.select('.vitals-table')[0].tbody.findAll('tr')[0].td.text
+		catchRate        = soup.select('.vitals-table')[1].tbody.findAll('tr')[1].td.text
+		gender           = soup.select('.vitals-table')[2].tbody.findAll('tr')[1].td.text
+		eggCycle         = soup.select('.vitals-table')[2].tbody.findAll('tr')[2].td.text
+		pokemonTypes     = []
+		pokemonAbilities = []
+		eggGroup         = []
+		evolution        = []
+		eggMoves         = []
+		tutorMoves       = []
+		hmMoves          = []
+		tmMoves          = []
+		transferMoves    = []
+		pokemonStats     = dict()
+		lvMoves          = dict()
+		location         = dict()
 
-		pokemonTypes = []
 		for item in soup.select('.vitals-table')[0].tbody.findAll('tr')[1].td.findAll('a'):
 			pokemonTypes.append(item.text)
 
-		pokemonAbilities = []
 		for item in soup.select('.vitals-table')[0].tbody.findAll('tr')[5].td.findAll('a'):
 			pokemonAbilities.append(item.text)  # last one is hidden ability
 
-		catchRate = soup.select('.vitals-table')[1].tbody.findAll('tr')[1].td.text
-
-		eggGroup = []
 		for item in soup.select('.vitals-table')[2].tbody.findAll('tr')[0].td.findAll('a'):
 			eggGroup.append(item.text)
 
-		gender   = soup.select('.vitals-table')[2].tbody.findAll('tr')[1].td.text
-		eggCycle = soup.select('.vitals-table')[2].tbody.findAll('tr')[2].td.text
-
-		pokemonStats              = {'hp': 0, 'attack': 0, 'defense': 0, 'spAttack': 0, 'spDefense': 0, 'speed': 0, 'total': 0}
 		pokemonStats['hp']        = soup.select('.vitals-table')[3].tbody.findAll('tr')[0].findAll('td')[0].text
 		pokemonStats['attack']    = soup.select('.vitals-table')[3].tbody.findAll('tr')[1].findAll('td')[0].text
 		pokemonStats['defense']   = soup.select('.vitals-table')[3].tbody.findAll('tr')[2].findAll('td')[0].text
@@ -46,38 +52,61 @@ class PokemonCrawler(scrapy.Spider):
 		pokemonStats['speed']     = soup.select('.vitals-table')[3].tbody.findAll('tr')[5].findAll('td')[0].text
 		pokemonStats['total']     = soup.select('.vitals-table')[3].tfoot.tr.td.text
 
-		# evolution = []
-		# for index, item in enumerate(soup.select('.infocard-evo-list')[0].findAll('span')):
-		# 	if index % 2 == 0:
-		# 		evolution.append(item.small.text[1:])
-		# 	else:
-		# 		evolution.append(item.text[12:14])
+		if soup.select('.infocard-evo-list') != []:
+			for index, item in enumerate(soup.select('.infocard-evo-list')[0].findAll('span')):
+				if index % 2 == 0:
+					evolution.append(item.small.text[1:])
+				else:
+					evolution.append(item.text[12:14])
 
-		lvMoves = dict()
-		for item in soup.select('.data-table')[0].tbody.findAll('tr'):
-			lvMoves[item.findAll('td')[0].text] = item.findAll('td')[1].text
+		# for tableIndex, dataTable in enumerate(soup.select('.data-table')):
+		# 	preSiblings = soup.select('.data-table')[tableIndex].previous_siblings
+		# 	for preSibling in preSiblings:
+		# 		if preSibling.name == 'h3':
+		# 			print preSibling.text
 
-		# eggMoves = []
+			# if preSibling is not None and preSibling.name is not None:
+			# 	print preSibling
+				# if nextSibling.text == "Egg moves":
+
+				# elif nextSibling.text == "Move Tutor moves":
+
+				# elif nextSibling.text == "Moves learnt by TM":
+
+				# elif nextSibling.text == "Egg moves":
+
+				# elif nextSibling.text == "Egg moves":
+
+				# elif nextSibling.text == "Egg moves":
+
+				# elif nextSibling.text == "Egg moves":
+
+				# elif nextSibling.text == "Egg moves":
+
+
+		# for item in soup.select('.data-table')[0].tbody.findAll('tr'):
+		# 	lvMoves[item.findAll('td')[0].text] = item.findAll('td')[1].text
+
+
 		# for item in soup.select('.data-table')[1].tbody.findAll('tr'):
 		# 	eggMoves.append(item.td.text)
 
-		tutorMoves = []
-		for item in soup.select('.data-table')[2].tbody.findAll('tr'):
-			tutorMoves.append(item.td.text)
 
-		# hmMoves = []
+		# for item in soup.select('.data-table')[2].tbody.findAll('tr'):
+		# 	tutorMoves.append(item.td.text)
+
+
 		# for item in soup.select('.data-table')[3].tbody.findAll('tr'):
 		# 	hmMoves.append(item.findAll('td')[1].text)
 
-		tmMoves = []
-		for item in soup.select('.data-table')[4].tbody.findAll('tr'):
-			tmMoves.append(item.findAll('td')[1].text)
 
-		transferMoves = []
-		for item in soup.select('.data-table')[5].tbody.findAll('tr'):
-			transferMoves.append(item.td.text)
+		# for item in soup.select('.data-table')[4].tbody.findAll('tr'):
+		# 	tmMoves.append(item.findAll('td')[1].text)
 
-		# location = dict()
+
+		# for item in soup.select('.data-table')[5].tbody.findAll('tr'):
+		# 	transferMoves.append(item.td.text)
+
 		# for item in soup.select('.vitals-table')[5].tbody.findAll('tr'):
 		# 	location[item.th.text] = item.td.text
 
@@ -87,11 +116,11 @@ class PokemonCrawler(scrapy.Spider):
 		print "Abilities:", pokemonAbilities
 		print "catchRate:", catchRate
 		print "Base Stats", pokemonStats
-		# print "Evolution Chain:", evolution
+		print "Evolution Chain:", evolution
 		print "Level Moves:", lvMoves
 		print "Egg Moves:", eggMoves
 		print "Tutor Moves:", tutorMoves
 		print "HM Moves:", hmMoves
 		print "TM Moves:", tmMoves
 		print "Transfer only Moves:", transferMoves
-		# print "Location:", location
+		print "Location:", location
